1db0e8bcfa1008edaabb9f04f60756953ba5fc13
LONEmi20 <micgredy@gmail.com>
2025-07-16 11:16:13 +0700

    homepage

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:tugas_kelompok_dpm/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const HomepagePendaftarFix());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
